d6055650a3858cc66a14d2842db077a705c82121
Danil Burtov <burtovdanil2005@gmail.com>
2025-03-18 17:05:21 +0300

    Добавьте файлы проекта.

---
+++ BurtovCheckOut/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using BurtovCheckOut.Components.Database;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BurtovCheckOut
+{
+	/// <summary>
+	/// Логика взаимодействия для App.xaml
+	/// </summary>
+	public partial class App : System.Windows.Application
+	{
+		public static User CurrentUser;
+		public static BurtovCheckOutEntities db = new BurtovCheckOutEntities();
+	}
+}
